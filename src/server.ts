0f9218856744631ec531bf78476fa6b5ca5dc192
Sam Jones <88912351+ssg406@users.noreply.github.com>
2023-05-25 00:03:36 -0700

    Removed CORS settings on HTTP server

--- src/server.ts
+++ src/server.ts
@@ -9,12 +9,7 @@ import cors from 'cors';
 const PORT = parseInt(<string>process.env.PORT) || 3000;
 
 const server = http.createServer();
-const io = new Server(server, {
-  cors: {
-    origin: 'http://localhost:10000',
-    methods: ['GET', 'POST'],
-  },
-});
+const io = new Server(server);
 
 // Start server
 server.listen(PORT);

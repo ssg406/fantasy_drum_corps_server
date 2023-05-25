483d67a30139090c2dccdbd7e3ebb369613c13d1
Sam Jones <88912351+ssg406@users.noreply.github.com>
2023-05-25 00:45:43 -0700

    Added express server and test get route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { Server } from 'socket.io';
 import http from 'http';
 import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
 // For local development
 // import * as dotenv from 'dotenv';
 
@@ -8,7 +9,15 @@ import cors from 'cors';
 
 const PORT = parseInt(<string>process.env.PORT) || 3000;
 
-const server = http.createServer();
+const app = express();
+
+app.use(cors());
+
+app.get('/test', function (req: Request, res: Response, next: NextFunction) {
+  res.status(200).send({ msg: 'This is a positive response' });
+});
+
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: 'http://localhost:1000',

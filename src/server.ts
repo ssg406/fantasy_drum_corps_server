7c2cf3b2b3c44a99d67eb3968808b23698d6433a
Sam Jones <sam@samueljones.codes>
2023-05-31 19:39:52 -0700

    added addTour route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { Server } from 'socket.io';
 import http from 'http';
 import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
+import { createNamespace } from 'index';
 // For local development
 // import * as dotenv from 'dotenv';
 
@@ -16,6 +17,12 @@ app.use(cors());
 app.get('/test', function (req: Request, res: Response, next: NextFunction) {
   res.status(200).send({ msg: 'This is a positive response' });
 });
+
+app.post('/addTour', function(req: Request, res: Response, next: NextFunction) {
+  const {tourId} = req.body;
+  createNamespace(tourId);
+  res.status(201);
+})
 
 const server = http.createServer(app);
 const io = new Server(server, {

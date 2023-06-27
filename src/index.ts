31414ba36083bfc2b9d599a5785743e554e9e110
Sam Jones <88912351+ssg406@users.noreply.github.com>
2023-06-27 00:26:33 -0700

    removed local development dotenv config

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import { toursRepository } from './data';
-//For local development
-import * as dotenv from 'dotenv';
+// For local development
+// import * as dotenv from 'dotenv';
+// dotenv.config();
+// End for local development
 import { createTourNamespace } from './createNamespace';
-
-dotenv.config();
 
 const PORT = parseInt(<string>process.env.PORT) || 3000;
 
